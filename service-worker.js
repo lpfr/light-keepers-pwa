d105043f5b6f441771adf87a71ee22ea447c9d4d
lpfr <103137254+lpfr@users.noreply.github.com>
2025-09-27 19:36:47 +0200

    service-worker-v3.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,31 @@
-
-const CACHE_NAME = 'light-keepers-v2';
+const CACHE_NAME = 'light-keepers-v3-' + Date.now();
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/light-keepers-pwa/',
+  '/light-keepers-pwa/index.html',
+  '/light-keepers-pwa/manifest.json',
+  '/light-keepers-pwa/icon-192.png',
+  '/light-keepers-pwa/icon-512.png'
 ];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request).then(r => {
-      const copy = r.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
-      return r;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(r =>
+      r || fetch(e.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+        return resp;
+      }).catch(() => caches.match('/light-keepers-pwa/index.html'))
+    )
   );
 });
 
